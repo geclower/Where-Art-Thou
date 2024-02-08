64672e1256f072cd652fca6f70c01a3d939fea6c
Grace Clower <clower.grace@gmail.com>
2024-02-08 11:52:38 -0500

    created a button to pull the next round of available media, adjusted the css

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("hello");
 
-document.querySelector("img").addEventListener("click", showPic);
+// document.querySelector("img").addEventListener("click", showPic);
 
-function showPic() {}
+// function showPic() {}
 
 const url = "https://streaming-availability.p.rapidapi.com";
 const countryList = document.querySelector("#country-select");
@@ -11,6 +11,7 @@ const genreList = document.querySelector("#genre-select");
 const mediaList = document.querySelector("#media-select");
 const submitBtn = document.querySelector(".submit");
 const contentList = document.querySelector(".results");
+const nextBtn = document.querySelector(".next");
 
 fetch(`${url}/countries`, {
   headers: {
@@ -83,73 +84,90 @@ function chooseService(e) {
 submitBtn.addEventListener("click", handleRequest);
 
 function handleRequest(e) {
-    e.preventDefault();
-    let w = countryList.value;
-    let x = genreList.value;
-    let y = mediaList.value;
-    let z = serviceList.value;
-  
-    fetch(
-      `${url}/search/filters?country=${w}&services=${z}&show_type=${y}&genres=${x}`,
-      {
-        headers: {
-          "X-RapidAPI-Key": "51743aff86msh302c2f53b560150p1eae9ejsn93fe2f9c4133",
-        },
-      }
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        let movies = data.result;
-        console.log(movies)
-        let link = "";
-  
-        for (let i = 0; i < movies.length; i++) {
-          let streamingOptions = movies[i].streamingInfo[w];
-          console.log("Streaming Options: ", streamingOptions)
+  e.preventDefault();
+  let w = countryList.value;
+  let x = genreList.value;
+  let y = mediaList.value;
+  let z = serviceList.value;
 
-          for (let j = 0; j < streamingOptions.length; j++) {
-            if (z === streamingOptions[j].service) {
-              link = streamingOptions[j].link;
-            }
+  fetch(
+    `${url}/search/filters?country=${w}&services=${z}&show_type=${y}&genres=${x}`,
+    {
+      headers: {
+        "X-RapidAPI-Key": "51743aff86msh302c2f53b560150p1eae9ejsn93fe2f9c4133",
+      },
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let movies = data.result;
+      console.log(data);
+      let link = "";
+      let cursor = "";
+
+      for (let i = 0; i < movies.length; i++) {
+        let streamingOptions = movies[i].streamingInfo[w];
+        for (let j = 0; j < streamingOptions?.length; j++) {
+          if (z === streamingOptions[j].service) {
+            link = streamingOptions[j].link;
           }
-          let mockHTML = `<li><a href=${link} target="blank">${movies[i].title}</a></li>`;
-          contentList.insertAdjacentHTML("beforeend", mockHTML);
         }
-      });
-  }
+        let mockHTML = `<li><a href=${link} target="blank">${movies[i].title}</a></li>`;
+        contentList.insertAdjacentHTML("beforeend", mockHTML);
+      }
+
+
+      if (data.hasMore) {
+        cursor = data.nextCursor;
+        nextBtn.style.opacity = 1;
+        nextBtn.addEventListener("click", nextRequest);
+
+        function nextRequest(e) {
+          e.preventDefault();
+
+          fetch(
+            `${url}/search/filters?country=${w}&services=${z}&show_type=${y}&genres=${x}&cursor=${cursor}`,
+            {
+              headers: {
+                "X-RapidAPI-Key":
+                  "51743aff86msh302c2f53b560150p1eae9ejsn93fe2f9c4133",
+              },
+            }
+          )
+            .then((res) => {
+              return res.json();
+            })
+            .then((data) => {
+              let movies = data.result;
+              console.log(data);
+              let link = "";
+              cursor = data.nextCursor;
+
+              for (let i = 0; i < movies.length; i++) {
+                let streamingOptions = movies[i].streamingInfo[w];
+                for (let j = 0; j < streamingOptions.length; j++) {
+                  if (z === streamingOptions[j].service) {
+                    link = streamingOptions[j].link;
+                  }
+                }
+                let mockHTML = `<li><a href=${link} target="blank">${movies[i].title}</a></li>`;
+                contentList.insertAdjacentHTML("beforeend", mockHTML);
+              }
+              
+            });
+           
+        }
+      }
+    });
+}
+
+// try to open results in second html page
 
 // if (window.location.pathname.includes("index.html")) {
 //   submitBtn.addEventListener("click", handleRequest);
 // }
-
-// // function handleRequest(e) {
-// //     e.preventDefault();
-
-// //     let w = countryList.value;
-// //     let x = genreList.value;
-// //     let y = mediaList.value;
-// //     let z = serviceList.value;
-
-// //     fetch(`${url}/country=${w}&services=${z}&show_type=${y}&genres=${x}`, {
-// //         headers: {
-// //           "X-RapidAPI-Key": "51743aff86msh302c2f53b560150p1eae9ejsn93fe2f9c4133",
-// //         },
-// //       })
-// //         .then((res) => {
-// //           return res.json();
-// //         })
-// //         .then((data) => {
-// //             let arr = Object.values(data.result);
-// //             for (i = 0; i < arr.length; i++) {
-// //                 let resultItem = document.createElement("li");
-// //                 resultItem.innerText = arr[i].title;
-// //                 resultItem.value = arr[i];
-// //                 contentList.appendChild(resultItem);
-// //               }
-// //         })
-// // }
 
 // function handleRequest(e) {
 //   e.preventDefault();
